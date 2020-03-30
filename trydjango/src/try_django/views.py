006c2ae35aa9b751276d8d22dfdc6c4e707fa99a
Your Name <you@example.com>
2020-03-30 23:08:40 +0300

    Commit

---
+++ trydjango/src/try_django/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+
+
+def home_page(request):
+	my_title = "Hello there ......."
+	# doc = "<h1>{title}</h1>".format(title=title)
+	# djano_rendered_doc = "<h1>{{title}}</h1>".format(title=title)
+	return render(request, "hello_world.html", {"title":my_title})
+
+
+def about_page(request):
+	return render(request, "about.html", {"title":"About Us"})
+
+
+def contact_page(request):
+	return render(request, "hello_world.html", {"title":"Contact Us"})
+
+def example_page(request):
+	context = {"title": "Example"}
+	template_name = "hello_world.html"
+	template_obj = get_template(template_name)
+
+	return HttpResponse(template_obj.render(context))                  
